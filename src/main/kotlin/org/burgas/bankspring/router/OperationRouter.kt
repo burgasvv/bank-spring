90ccef86bf769abfc231f2f753d55b2ea700b0fd
Burgas <burgassme@gmail.com>
2026-04-19 18:56:24 +0700

    Adding transfer filter

--- src/main/kotlin/org/burgas/bankspring/router/OperationRouter.kt
+++ src/main/kotlin/org/burgas/bankspring/router/OperationRouter.kt
@@ -1,25 +1,73 @@
 package org.burgas.bankspring.router
 
+import org.burgas.bankspring.dao.identity.IdentityDetails
 import org.burgas.bankspring.dto.exception.ExceptionResponse
 import org.burgas.bankspring.dto.operation.OperationRequest
 import org.burgas.bankspring.router.contract.Router
+import org.burgas.bankspring.service.CardService
 import org.burgas.bankspring.service.OperationService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.security.core.Authentication
 import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.attributeOrNull
 import org.springframework.web.servlet.function.body
 import org.springframework.web.servlet.function.paramOrNull
+import org.springframework.web.servlet.function.principalOrNull
 import org.springframework.web.servlet.function.router
 import java.util.UUID
 
 @Configuration
-class OperationRouter(override val service: OperationService) : Router<OperationService> {
+class OperationRouter(override val service: OperationService, val cardService: CardService) : Router<OperationService> {
 
     @Bean
     fun operationRoutes() = router {
         "/api/v1/operations".nest {
+
+            filter { request, function ->
+                if (request.path().equals("/api/v1/operations/by-id", false)) {
+                    val authentication = request.principalOrNull() as Authentication
+
+                    if (authentication.isAuthenticated) {
+                        val identityDetails = authentication.principal as IdentityDetails
+                        val operationId = UUID.fromString(request.paramOrNull("operationId"))
+                        val operation = service.findEntity(operationId)
+
+                        if (operation.card?.account?.wallet?.identity?.id == identityDetails.identity.id) {
+                            function(request)
+                        } else {
+                            throw IllegalArgumentException("Identity not authorized")
+                        }
+
+                    } else {
+                        throw IllegalArgumentException("Identity not authenticated")
+                    }
+
+                } else if (request.path().equals("/api/v1/operations/create", false)) {
+                    val authentication = request.principalOrNull() as Authentication
+
+                    if (authentication.isAuthenticated) {
+                        val identityDetails = authentication.principal as IdentityDetails
+                        val operationRequest = request.body<OperationRequest>()
+                        val card = cardService.findEntity(operationRequest.cardId)
+
+                        if (card.account?.wallet?.identity?.id == identityDetails.identity.id) {
+                            request.attributes()["operationRequest"] = operationRequest
+                            function(request)
+                        } else {
+                            throw IllegalArgumentException("Identity not authorized")
+                        }
+
+                    } else {
+                        throw IllegalArgumentException("Identity not authenticated")
+                    }
+
+                } else {
+                    function(request)
+                }
+            }
 
             GET("/by-id") {
                 val operationId = UUID.fromString(it.paramOrNull("operationId"))
@@ -30,7 +78,7 @@ class OperationRouter(override val service: OperationService) : Router<Operation
             }
 
             POST("/create") {
-                val operationRequest = it.body<OperationRequest>()
+                val operationRequest = it.attributeOrNull("operationRequest") as OperationRequest
                 service.create(operationRequest)
                 ServerResponse.status(HttpStatus.OK).build()
             }
--- src/main/kotlin/org/burgas/bankspring/router/TransferRouter.kt
+++ src/main/kotlin/org/burgas/bankspring/router/TransferRouter.kt
@@ -1,25 +1,73 @@
 package org.burgas.bankspring.router
 
+import org.burgas.bankspring.dao.identity.IdentityDetails
 import org.burgas.bankspring.dto.exception.ExceptionResponse
 import org.burgas.bankspring.dto.transfer.TransferRequest
 import org.burgas.bankspring.router.contract.Router
+import org.burgas.bankspring.service.CardService
 import org.burgas.bankspring.service.TransferService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.security.core.Authentication
 import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.attributeOrNull
 import org.springframework.web.servlet.function.body
 import org.springframework.web.servlet.function.paramOrNull
+import org.springframework.web.servlet.function.principalOrNull
 import org.springframework.web.servlet.function.router
 import java.util.UUID
 
 @Configuration
-class TransferRouter(override val service: TransferService) : Router<TransferService> {
+class TransferRouter(override val service: TransferService, val cardService: CardService) : Router<TransferService> {
 
     @Bean
     fun transferRoutes() = router {
         "/api/v1/transfers".nest {
+
+            filter { request, function ->
+                if (request.path().equals("/api/v1/transfers/by-id", false)) {
+                    val authentication = request.principalOrNull() as Authentication
+
+                    if (authentication.isAuthenticated) {
+                        val identityDetails = authentication.principal as IdentityDetails
+                        val transferId = UUID.fromString(request.paramOrNull("transferId"))
+                        val transfer = service.findEntity(transferId)
+
+                        if (transfer.sender?.account?.wallet?.identity?.id == identityDetails.identity.id) {
+                            function(request)
+                        } else {
+                            throw IllegalArgumentException("Identity not authorized")
+                        }
+
+                    } else {
+                        throw IllegalArgumentException("identity not authenticated")
+                    }
+
+                } else if (request.path().equals("/api/v1/transfers/create", false)) {
+                    val authentication = request.principalOrNull() as Authentication
+
+                    if (authentication.isAuthenticated) {
+                        val identityDetails = authentication.principal as IdentityDetails
+                        val transferRequest = request.body<TransferRequest>()
+                        val sender = cardService.findEntity(transferRequest.senderId)
+
+                        if (sender.account?.wallet?.identity?.id == identityDetails.identity.id) {
+                            request.attributes()["transferRequest"] = transferRequest
+                            function(request)
+                        } else {
+                            throw IllegalArgumentException("Identity not authorized")
+                        }
+
+                    } else {
+                        throw IllegalArgumentException("Identity not authenticated")
+                    }
+
+                } else {
+                    function(request)
+                }
+            }
 
             GET("/by-id") {
                 val transferId = UUID.fromString(it.paramOrNull("transferId"))
@@ -30,7 +78,7 @@ class TransferRouter(override val service: TransferService) : Router<TransferSer
             }
 
             POST("/create") {
-                val transferRequest = it.body<TransferRequest>()
+                val transferRequest = it.attributeOrNull("transferRequest") as TransferRequest
                 service.create(transferRequest)
                 ServerResponse.status(HttpStatus.OK).build()
             }
